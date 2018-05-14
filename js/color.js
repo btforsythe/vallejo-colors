793212fc8e676e4ec198e8016dfbd0de95d252c1
B T Forsythe <brian@btforsythe.io>
2018-05-13 21:34:10 -0400

    moved color methods to prototype. oops

--- js/color.js
+++ js/color.js
@@ -6,22 +6,24 @@ function Color(sourceString) {
 	this.red = parseInt(results[0])
 	this.green = parseInt(results[1])
 	this.blue = parseInt(results[2])
+}
 
-	this.similarityWith = (other) => 100 - this.differenceWith(other)
+Color.prototype.differenceWith = function(other) {
+	var distance = Math.sqrt(Math.pow(this.red - other.red, 2)
+		+ Math.pow(this.green - other.green, 2)
+		+ Math.pow(this.blue - other.blue, 2))
+	return Math.round(distance / MAX_DISTANCE * 100)
+}
 
-	this.differenceWith = (other) => {
-		var distance = Math.sqrt(Math.pow(this.red - other.red, 2)
-			+ Math.pow(this.green - other.green, 2)
-			+ Math.pow(this.blue - other.blue, 2))
-		return Math.round(distance / MAX_DISTANCE * 100)
+Color.prototype.similarityWith = function(other) {
+	return 100 - this.differenceWith(other)
+}
+
+Color.prototype.matches = function(other) {
+	if(other instanceof Color) {
+		return this.red == other.red
+		&& this.green == other.green
+		&& this.blue == other.blue
 	}
-
-	this.matches = (other) => {
-		if(other instanceof Color) {
-			return this.red == other.red
-				&& this.green == other.green
-				&& this.blue == other.blue
-		}
-		return this.matches(new Color(other))
-	}
+	return this.matches(new Color(other))
 }
