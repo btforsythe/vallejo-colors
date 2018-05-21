eee3b036ac1bea5981e11da03b95a12ca8b20421
B T Forsythe <brian@btforsythe.io>
2018-05-20 21:12:04 -0400

    finding products belonging to multiple sets

--- neo4j/population/js/populate-products.js
+++ neo4j/population/js/populate-products.js
@@ -1,15 +1,17 @@
 window.onload = function() {
 	findNon70Products()
+	findDuplicatedProducts()
+	writeProductCreationQueries()
 }
 
 const findNon70Products = function() {
 	sets.forEach(s => s.productsByModel.forEach(
-		(p) => p.model.startsWith('70')? {}: writeNon70Product(s, p))
+		(p) => p.model.startsWith('70')? {}: writeProblemProductRow(s, p, '.non-70-models'))
 	)
 }
 
-const writeNon70Product = function(set, product) {
-	let tr = document.querySelector('.non-70-models').appendChild(document.createElement('tr'))
+const writeProblemProductRow = function(set, product, parentSelector) {
+	let tr = document.querySelector(parentSelector).appendChild(document.createElement('tr'))
 	appendTd(tr, set.name)
 	appendTd(tr, product.name)
 	appendTd(tr, product.model)
@@ -20,3 +22,52 @@ const writeNon70Product = function(set, product) {
 
 const appendTd =
 	(tr, text) => tr.appendChild(document.createElement('td')).innerText = text
+
+const findDuplicatedProducts = function() {
+
+	var productModelNumbers = []
+	var duplicateModelNumbers = []
+	sets.forEach(function(set) {
+		Array.from(set.productsByModel.keys()).forEach(function(modelNumber) {
+			if(productModelNumbers.includes(modelNumber)) {
+				if(!duplicateModelNumbers.includes(modelNumber)) {
+					duplicateModelNumbers.push(modelNumber)
+				}
+			} else {
+				productModelNumbers.push(modelNumber)
+			}
+		})
+	})
+	duplicateModelNumbers.forEach(duplicate => {
+		forEverySetAndProduct(
+			(set, product, modelNumber) => {
+				if(duplicate === modelNumber) {
+					writeProblemProductRow(set, product, '.duplicatedProducts')
+				}
+			}
+		)
+	})
+}
+
+const writeProductCreationQueries = function() {
+	var queries = []
+	forEverySetAndProduct((s, p) => queries.push(buildCreateProductQuery(s, p)))
+	populateQueries('.productCreationQueries', queries)
+}
+
+const forEverySetAndProduct = function(callback) {
+	sets.forEach(function(set) {
+		set.productsByModel.forEach(function(product, productModelNumber) {
+			callback(set, product, productModelNumber)
+		})
+	})
+}
+
+const buildCreateProductQuery = (s, p) => `match (:Set {modelNumber:"${s.model}"})<-[:belongs_to]-(:Product ${buildProduct(p)})`
+
+const buildProduct = (p) =>
+	p.color === Color.UNCOLORED?
+		`{name:"${p.name}", modelNumber:"${p.model}"}`:
+		`{name:"${p.name}", modelNumber:"${p.model}", ${buildRgb(p.color)}}`
+
+const buildRgb = (c) => `red:${c.red}, green:${c.green}, blue:${c.blue}`
